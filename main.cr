102758622b1d3ca97b48963b76ee7da52124e014
Solomon Jackson <sugarfi@sugarfi.dev>
2020-09-01 17:48:51 -0400

    port backend to cr

---
+++ main.cr
@@ -0,0 +1,38 @@
+require "kemal"
+require "yaml"
+
+def get_posts
+    posts = [] of { String, Time, String, String }
+    Dir.new("blog").each do |post|
+        unless post == "." || post == ".."
+            file = post.split('.')[0]
+            post = YAML.parse File.read("blog/#{post}")
+            posts << { post["title"].as_s, Time.parse_iso8601(post["time"].as_s), post["body"].as_s, file }
+        end
+    end
+    posts
+end
+
+get "/" do
+    posts = get_posts
+    posts.sort_by! { |x| x[1] }
+    posts.reverse!
+    render "public/index.ecr"
+end
+
+get "/blog/:post" do |env|
+    posts = get_posts
+    posts.reject! { |x| x[3] != env.params.url["post"] }
+    if posts.size > 0
+        post = posts[0]
+        render "public/post.ecr"
+    else
+        render "public/404.ecr"
+    end
+end
+
+error 404 do
+    render "public/404.ecr"
+end
+
+Kemal.run
